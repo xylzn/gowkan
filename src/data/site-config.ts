be9fad7575c527b427c51a45af936353e95bc62b
Dedi Rialdi As Aldi <143459592+xylzn@users.noreply.github.com>
2026-09-07 23:45:53 +0700

    update OG n Favicon

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,9 +1,29 @@
 export const SITE_CONFIG = {
     name: "GOWKAN",
     description: "Jasa Pembuatan Website Modern & Elegant",
+    siteUrl: "https://gowkan.my.id",
+    locale: "id_ID",
+    themeColor: "#6366f1",
     logo: {
         text: "GOW",
         highlight: "KAN",
+    },
+    seo: {
+        defaultOgImage: "/og-image.jpg",
+        ogImageAlt: "GOWKAN - Jasa Pembuatan Website Modern & Elegant",
+        ogImageWidth: 1200,
+        ogImageHeight: 630,
+        twitterSite: "@gowkan",
+        twitterCreator: "@gowkan",
+    },
+    favicon: {
+        svg: "/favicon.svg",
+        ico: "/favicon.ico",
+        png16: "/favicon-16x16.png",
+        png32: "/favicon-32x32.png",
+        appleTouch: "/apple-touch-icon.png",
+        safariPinnedTab: "/safari-pinned-tab.svg",
+        webmanifest: "/site.webmanifest",
     },
     contact: {
         email: "gowkancompany@gmail.com",
